5ef68064cac693ae8fa868633f940f6fd06662c0
h671434 <spjort2003@gmail.com>
2024-02-26 15:27:04 +0100

    init, Task A and B1

---
+++ dat110-project2-mom-startcode/src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -0,0 +1,26 @@
+package no.hvl.dat110.messages;
+
+public class DeleteTopicMsg extends Message {
+
+	// message sent from client to create topic on the broker
+	private String topic;
+	
+    public DeleteTopicMsg(String user, String topic) {
+    	super(MessageType.DELETETOPIC, user);
+    	this.topic = topic;
+    }
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [type=" + getType() + ", user=" + getUser() + ", topic=" + topic + "]";
+	}
+	
+}
