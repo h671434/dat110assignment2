0bfc9a226152bd9f603a4cccc5f3a7a1b8d91cba
h671434 <111515930+h671434@users.noreply.github.com>
2024-03-07 16:27:29 +0100

    Delete dat110-project2-mom-startcode directory

--- dat110-project2-mom-startcode/src/main/java/no/hvl/dat110/messages/MessageType.java
+++
@@ -1,7 +0,0 @@
-package no.hvl.dat110.messages;
-
-public enum MessageType {
-
-	CONNECT, DISCONNECT, SUBSCRIBE, UNSUBSCRIBE, PUBLISH, CREATETOPIC, DELETETOPIC, STATUS;
-	
-}
--- dat110-project2-mom-startcode/src/main/java/no/hvl/dat110/messagetransport/MessageConfig.java
+++
@@ -1,9 +0,0 @@
-package no.hvl.dat110.messagetransport;
-
-public class MessageConfig {
-
-	public static final int SEGMENTSIZE = 128;
-	
-	public static final int MESSAGINGPORT = 8080;
-	public static final String MESSAGINGHOST = "localhost";
-}
